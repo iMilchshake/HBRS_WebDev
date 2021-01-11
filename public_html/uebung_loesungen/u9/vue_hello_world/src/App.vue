91ba9f9903da3a1e3f913b3b99dad8280635a980
Tobias Schneider <tobias.schneider@smail.inf.h-brs.de>
2021-01-11 18:35:20 +0100

    vue.js test_project

---
+++ public_html/uebung_loesungen/u9/vue_hello_world/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <eingabe/>
+  </div>
+</template>
+
+<script>
+import eingabe from "@/components/eingabe";
+
+export default {
+  name: 'App',
+  components: {
+    eingabe
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ public_html/uebung_loesungen/u9/vue_hello_world/src/components/eingabe.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <p> this is text {{ 3 + 3 }} :) </p>
+    <input v-model="message" placeholder="edit me">
+    <p>Message is: {{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "eingabe.vue",
+  data: function () {
+    return {
+      message: "message default"
+    }
+  }
+}
+
+
+</script>
+
+<style scoped>
+p {
+  color: red;
+}
+</style>
