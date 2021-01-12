02b8e1e11b1ba98bd24f405c85e0331d8f1cd497
Tobias Schneider <tobias.schneider@smail.inf.h-brs.de>
2021-01-12 15:06:08 +0100

    finished u9/a2

---
+++ public_html/uebung_loesungen/u9/a2/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h2> Menu horizontal</h2>
+    <Menu v-bind:elements="['Menu1', 'Menu2', 'Menu3', 'Menu4']" direction="row"/>
+
+    <br>
+    <h2> Menu vertical </h2>
+    <Menu v-bind:elements="['Menu1', 'Menu2', 'Menu3', 'Menu4']" direction="column"/>
+  </div>
+</template>
+
+<script>
+import Menu from "./components/Menu";
+
+export default {
+  name: 'App',
+  components: {
+    Menu
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ public_html/uebung_loesungen/u9/a2/src/components/Menu.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="menu" :style="cssVars">
+    <button type="button" class="button" v-for="item in elements" :key="item.id"> {{ item }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Menu",
+  props: {
+    elements: Array,
+    direction: String
+  },
+  computed: {
+    cssVars() {
+      return {
+        '--dir': this.direction,
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.menu {
+  display: flex;
+  flex-direction: var(--dir);
+}
+
+.button {
+  background-color: #646464;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: block;
+  font-size: 16px;
+  height: 100%;
+  width: 200px;
+}
+
+.button:hover {
+  background-color: #7c7c7c;
+}
+
+</style>
