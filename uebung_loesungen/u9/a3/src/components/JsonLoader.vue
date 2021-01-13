8ea1289f15a3dcde49cef53651f2f1c97db3c8cb
Tobias Schneider <tobias.schneider@smail.inf.h-brs.de>
2021-01-13 15:21:56 +0100

    changed project structure

---
+++ uebung_loesungen/u9/a3/src/components/JsonLoader.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <p> THIS WILL BE THE DYNAMIC SITE ! </p>
+  </div>
+</template>
+
+<script>
+
+// import json data
+import json_data from "../assets/data";
+import Home from "../views/Home";
+
+// Vue Object
+export default {
+  name: "JsonLoader",
+  created() {
+    console.log("json data: ", json_data)
+
+    // generate paths
+    Object.keys(json_data["data"]).forEach(x => {
+      this.$router.addRoutes([{
+        path: '/'+x,
+        name: x,
+        component: Home
+      }]);
+      console.log(x);
+    })
+    console.log(this.$router.currentRoute);
+    this.j = json_data;
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ uebung_loesungen/u9/a3/src/views/TestPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <p> Hello World :) </p>
+    <p>{{ count }}</p>
+    <p>
+      <button @click="increment">+</button>
+      <button @click="reset">0</button>
+    </p>
+    <JsonLoader/>
+  </div>
+</template>
+
+<script>
+import JsonLoader from "../components/JsonLoader";
+
+export default {
+  name: "TestPage",
+  components: {JsonLoader},
+  computed: {
+    count() {
+      return this.$store.state.count
+    }
+  },
+  methods: {
+    increment() {
+      this.$store.commit('increment')
+    },
+    reset() {
+      this.$store.commit('reset')
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
