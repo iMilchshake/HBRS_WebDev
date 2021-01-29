5b3df69b47d6b429f886f30f3262f76e464ff2e7
Tobias Schneider <tobias.schneider@smail.inf.h-brs.de>
2021-01-29 12:07:56 +0100

    added content to welcome page

--- finalhausarbeitvue/src/views/welcome/Welcome.vue
+++ finalhausarbeitvue/src/views/welcome/Welcome.vue
@@ -1,16 +1,65 @@
 <template>
-<div id="welcome">
-  <h1> Welcome ! </h1>
-  <router-link to="/site/home"> >> click here to visit my site </router-link>
-</div>
+  <div id="welcome" class="bg">
+    <div class="center">
+      <div class="caption">
+        <h1> Hey, im Tobi! </h1>
+        <button type="button" class="welcome_button" @click="$router.push('/site/home')"> check out my site</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-name: "Welcome"
+  name: "Welcome"
 }
 </script>
 
 <style scoped>
 
+.welcome_button {
+  background-color: rgba(0, 0, 0, 1);
+  border: none;
+  outline: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  height: 100%;
+  border-radius: 4px;
+}
+
+.bg {
+  background-image: url("../../assets/code_blurred.jpg");
+  /*filter: blur(12px);
+  -webkit-filter: blur(12px);
+*/
+  width: 100%;
+  min-height: 100vh;
+
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  box-sizing: border-box;
+}
+
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  width: 100%;
+}
+
+.caption {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  font-weight: bold;
+  text-align: center;
+}
 </style>
