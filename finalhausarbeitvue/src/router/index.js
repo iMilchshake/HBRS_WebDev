5d8bf8b11a7ea2f3b29ee7b2282dd80268fd2dd2
Tobias Schneider <tobias.schneider@smail.inf.h-brs.de>
2021-02-02 11:50:59 +0100

    added 404(NotFound) page

--- finalhausarbeitvue/src/router/index.js
+++ finalhausarbeitvue/src/router/index.js
@@ -1,19 +1,30 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
 import Home from "../views/site/home_wrapper/Home";
 import About from "../views/site/about_wrapper/about/About";
-import Main from "../views/site/Site";
 import Welcome from "../views/welcome/Welcome";
 import About_wrapper from "../views/site/about_wrapper/About_wrapper";
 import About2 from "../views/site/about_wrapper/about2/About2";
 import Navigator_wrapper from "../views/site/navigator_wrapper/Navigator_wrapper";
 import Solution_wrapper from "../views/solutions_wrapper/Solution_wrapper";
 import u1_a4 from "../views/solutions_wrapper/solutions/u1_a4";
+import Site from "../views/site/Site";
+import NotFound from "../views/NotFound/NotFound";
 
 const routes = [
     {
+        path: '/',
+        name: 'Welcome',
+        component: Welcome
+    },
+    {
+        path: "/:catchAll(.*)",
+        name: 'NotFoundError',
+        component: NotFound
+    },
+    {
         path: '/site',
         name: 'Site',
-        component: Main,
+        component: Site,
         children: [
             {
                 path: '/site/home',
@@ -42,11 +53,6 @@ const routes = [
                 component: Navigator_wrapper,
                 children: []
             }]
-    },
-    {
-        path: '/',
-        name: 'Welcome',
-        component: Welcome
     },
     {
         path: '/solutions_wrapper/',
---
+++ finalhausarbeitvue/src/views/NotFound/NotFound.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="NotFound" class="bg">
+    <div class="center">
+      <div class="caption">
+        <h1> Hey, you got lost! </h1>
+        <button type="button" class="welcome_button" @click="$router.push('/site/home')"> return to main page </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound"
+}
+</script>
+
+<style scoped>
+
+.welcome_button {
+  background-color: rgba(0, 0, 0, 1);
+  border: none;
+  outline: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  height: 100%;
+  border-radius: 4px;
+}
+
+.bg {
+  background-image: url("../../assets/code_blurred.jpg");
+  /*filter: blur(12px);
+  -webkit-filter: blur(12px);
+*/
+  width: 100%;
+  min-height: 100vh;
+
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  box-sizing: border-box;
+}
+
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  width: 100%;
+}
+
+.caption {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  font-weight: bold;
+  text-align: center;
+}
+</style>
