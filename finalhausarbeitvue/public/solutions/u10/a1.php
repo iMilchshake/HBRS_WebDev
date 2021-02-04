90c7d03588c172ebc19c9b7ceac2c5ea158da795
Tobias Schneider <tobias.schneider@smail.inf.h-brs.de>
2021-02-04 10:53:43 +0100

    added solution for exercise 10

---
+++ finalhausarbeitvue/public/solutions/u10/a1.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Register</title>
+</head>
+<style>
+    fieldset {
+        width: 1px;
+    }
+</style>
+<body>
+
+<form method="post">
+    <fieldset>
+        <label for="user">user:</label><br>
+        <input type="text" id="user" name="user"><br>
+        <label for="pass">pass:</label><br>
+        <input type="text" id="pass" name="pass"><br><br>
+        <input type="submit" id="button" value="Submit">
+    </fieldset>
+</form>
+
+<?php
+    if(isset($_POST['user']) && isset($_POST['pass'])) {
+        $user = $_POST['user'];
+        $pass = $_POST['pass'];
+
+        $file = './raw_passwords.csv';
+        $new_line = $user . ',' . $pass . "\n";
+
+        if(file_put_contents($file, $new_line, FILE_APPEND | LOCK_EX)) {
+            echo "<script> alert('added new user')</script>";
+        } else {
+            echo "<script> alert('error while adding new user!')</script>";
+        }
+
+    } else {
+    }
+?>
+
+</body>
+</html>
