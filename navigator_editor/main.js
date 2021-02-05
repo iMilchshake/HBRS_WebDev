3a41f6937249f69dad3942804ed3b2ddc6b2b0d6
Tobias Schneider <tobias.schneider@smail.inf.h-brs.de>
2021-02-05 15:36:39 +0100

    fixed some bugs

--- navigator_editor/main.js
+++ navigator_editor/main.js
@@ -5,13 +5,17 @@ const select_unteraufgabe = document.getElementById("select_unteraufgabe");
 let uebung_data;
 let aufgaben_data;
 let unteraufgaben_data;
+let current_uebung;
+let current_aufgabe;
+let current_unteraufgabe;
+let json;
 
 // initialize
 let promise = initializeEditor();
 
 // fetch json data from server and display editor
 async function initializeEditor() {
-    const json = await fetch('file.json').then(x => x.json());
+    json = await fetch('file.json').then(x => x.json());
     console.log("fetched data:", json);
 
     json.forEach(uebung => {
@@ -19,11 +23,11 @@ async function initializeEditor() {
     });
 
     // add event handlers for UI
-    select_uebung.addEventListener("change", id => updateSelects('select_uebung', json));
-    select_aufgabe.addEventListener("change", id => updateSelects('select_aufgabe', json));
-    select_unteraufgabe.addEventListener("change", id => updateSelects('select_unteraufgabe', json));
-    document.getElementById('send_button').addEventListener("click", x => sendData(json));
-    document.getElementById('button_adduebung').addEventListener("click", x => addNewEntity("uebung", json));
+    select_uebung.addEventListener("change", id => updateSelects('select_uebung'));
+    select_aufgabe.addEventListener("change", id => updateSelects('select_aufgabe'));
+    select_unteraufgabe.addEventListener("change", id => updateSelects('select_unteraufgabe'));
+    document.getElementById('send_button').addEventListener("click", x => sendData());
+    document.getElementById('button_adduebung').addEventListener("click", x => addNewEntity("uebung"));
 }
 
 function addOptionToSelect(option_name, select_id) {
@@ -38,23 +42,27 @@ function removeAllOptions(select) {
 }
 
 function getCurrentSelection(select) {
-    return select.options[select.selectedIndex].value;
+    if (select.selectedIndex !== -1)
+        return select.options[select.selectedIndex].value;
+    else
+        return undefined
 }
 
-function addNewEntity(type, json) {
+function addNewEntity(type) {
     switch (type) {
         case "uebung":
             const name = prompt("Name der Übung?", "Übung X");
             json.push({exercise: name, heading: "", tasks: []})
             console.log(json);
-            select_uebung.selectedIndex = select_uebung.childElementCount - 1;
 
             removeAllOptions(select_uebung);
             json.forEach(uebung => {
                 addOptionToSelect(uebung['exercise'], "select_uebung")
             });
 
+            select_uebung.selectedIndex = select_uebung.childElementCount - 1;
             updateSelects('select_uebung', json);
+
     }
 }
 
@@ -67,20 +75,24 @@ function updateAufgabenDisplay() {
 }
 
 function updateUnteraufgabenDisplay() {
-    document.getElementById('edit_q').value = unteraufgaben_data.q;
-    if (unteraufgaben_data['q_extra']) document.getElementById('edit_q_extra').value = unteraufgaben_data.q_extra;
-    if (unteraufgaben_data['a']) document.getElementById('edit_a').value = unteraufgaben_data.a;
-    if (unteraufgaben_data['src']) document.getElementById('edit_src').value = unteraufgaben_data.src;
-    if (unteraufgaben_data['path']) document.getElementById('edit_path').value = unteraufgaben_data.path;
-    if (unteraufgaben_data['route']) document.getElementById('edit_route').value = unteraufgaben_data.route;
-}
+    const dom_ids = ['edit_q', 'edit_q_extra', 'edit_a', 'edit_src', 'edit_path', 'edit_route'];
+    const json_ids = ['q', 'q_extra', 'a', 'src', 'path', 'route'];
 
-function updateSelects(select_id, json) {
+    dom_ids.forEach((id, i) => {
+        const txt = document.getElementById(id);
+        if(unteraufgaben_data[json_ids[i]]) {
+            txt.value = unteraufgaben_data[json_ids[i]];
+        } else {
+            txt.value = "";
+        }
+    })
+
+function updateSelects(select_id) {
     switch (select_id) {
         case 'select_uebung':
             // get selection
-            const uebung = getCurrentSelection(select_uebung);
-            uebung_data = json.find(u => u['exercise'] === uebung);
+            current_uebung = getCurrentSelection(select_uebung);
+            uebung_data = json.find(u => u['exercise'] === current_uebung);
 
             // update select_aufgabe
             removeAllOptions(select_aufgabe);
@@ -93,25 +105,30 @@ function updateSelects(select_id, json) {
 
         case 'select_aufgabe':
             // get selection
-            const aufgabe = getCurrentSelection(select_aufgabe);
-            aufgaben_data = uebung_data['tasks'].find(a => a['task'] === aufgabe);
+            current_aufgabe = getCurrentSelection(select_aufgabe);
 
-            // update select_unteraufgabe
-            removeAllOptions(select_unteraufgabe);
-            aufgaben_data['subtasks'].forEach((subtask, i) => {
-                addOptionToSelect(i, 'select_unteraufgabe');
-            });
+            if (current_aufgabe) {
+                aufgaben_data = uebung_data['tasks'].find(a => a['task'] === current_aufgabe);
 
-            // update display_aufgaben
-            updateAufgabenDisplay();
+                // update select_unteraufgabe
+                removeAllOptions(select_unteraufgabe);
+                aufgaben_data['subtasks'].forEach((subtask, i) => {
+                    addOptionToSelect(i, 'select_unteraufgabe');
+                });
 
+                // update display_aufgaben
+                updateAufgabenDisplay();
+            }
         case 'select_unteraufgabe':
             // get selection
-            const unteraufgabe = getCurrentSelection(select_unteraufgabe);
-            unteraufgaben_data = aufgaben_data['subtasks'][unteraufgabe]
+            current_unteraufgabe = getCurrentSelection(select_unteraufgabe);
 
-            // update display_unteraufgaben
-            updateUnteraufgabenDisplay();
+            if (current_unteraufgabe) {
+                unteraufgaben_data = aufgaben_data['subtasks'][current_unteraufgabe]
+
+                // update display_unteraufgaben
+                updateUnteraufgabenDisplay();
+            }
     }
     //console.log(uebung_data, aufgaben_data, unteraufgaben_data);
 }
