8630a8ff379675cc57705d3cfea026844dbdf2d5
Tobias Schneider <tobias.schneider@smail.inf.h-brs.de>
2021-02-07 20:44:29 +0100

    improved security of login

--- finalhausarbeitvue/public/editor/login.php
+++ finalhausarbeitvue/public/editor/login.php
@@ -1,12 +1,35 @@
 <?php
 
+// secrets are outside of public folder
+include("../../secret.php");
+
+function hashInput($inp)
+{
+    global $salt;
+    return hash("sha384", $inp . $salt, false);
+}
+
+function testUser($user, $pass)
+{
+    global $logins;
+    foreach($logins as $index => $login) {
+        if(hashInput($user) === $login["user"] && hashInput($pass) === $login["pass"]) {
+            return true;
+        }
+    }
+    return false;
+}
+
 // receive and parse json data
 header("Content-Type: application/json");
 $data = file_get_contents("php://input");
 $json = json_decode($data, true);
 
+// start session
 session_start();
-if($json['user'] === "admin" && $json['pass'] === "admin") {
+
+// test user login
+if(testUser($json["user"],$json["pass"])) {
     // start a session to save login-state
     $_SESSION["login"] = True;
     echo "{\"status\": \"success\"}";
---
+++ finalhausarbeitvue/secret.php
@@ -0,0 +1,13 @@
+<?php
+$salt = "salt456";
+$logins = array(
+    array(
+        "user" => "1b17e7e809a44b78bab64701ba0dbaa1ab3d033718336bd145b699f252295929a2d28af1e0ad9bac19e6d9db81092ad4",
+        "pass" => "3c8811136517d6367e60a4080f98e276d3abeed245a70cb193565a297d9b8629d9a486f3b18db3f138ec2468adcdaf38",
+    ),
+    array(
+        "user" => "feb9a7100eb9df29bfd4b34d80e6417faee4c37bb4120764de8ef6a5dc3669900d6eb45dbbec3c9f4c963920539af2aa",
+        "pass" => "a456f53f8ab846e3db9eda93529152f381944bc217716f906d426e10ab73688abec9f1a5d4a190a20cd4c893afa040f7",
+    )
+);
+?>
