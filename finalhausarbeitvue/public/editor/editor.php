cddcf6ce45985ec4e46b7507c42cc7b642ad8115
Tobias Schneider <tobias.schneider@smail.inf.h-brs.de>
2021-02-10 15:04:36 +0100

    added additional error handling for editor.php

--- finalhausarbeitvue/public/editor/editor.php
+++ finalhausarbeitvue/public/editor/editor.php
@@ -12,22 +12,31 @@ else {
     // receive and parse json data
     header("Content-Type: application/json");
     $data = file_get_contents("php://input");
-    $json = json_decode($data, true);
 
-    // catch errors
-    $jsonError = json_last_error();
-    $jsonErrorMsg = json_last_error_msg();
-
-    // send answer and save json
-    if ($jsonError === 0) { // success
-        $file_content = json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-        file_put_contents("../exercises.json", $file_content);
-        echo "{\"status\": \"success\"}";
-    } else { // error
+    // validate if sent data is not empty
+    if($data === "") {
         echo "{\"status\": \"error\",
-         \"error\": \"$jsonError\",
-          \"msg\": \"$jsonErrorMsg\",
+          \"msg\": \"no content was sent\",
            \"received-data\": \"$data\"}";
+    } else {
+        // decode json
+        $json = json_decode($data, true);
+
+        // catch errors
+        $jsonError = json_last_error();
+        $jsonErrorMsg = json_last_error_msg();
+
+        // send answer and save json
+        if ($jsonError === 0) { // success
+            $file_content = json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+            file_put_contents("../exercises.json", $file_content);
+            echo "{\"status\": \"success\"}";
+        } else { // error
+            echo "{\"status\": \"error\",
+             \"error\": \"$jsonError\",
+              \"msg\": \"$jsonErrorMsg\",
+               \"received-data\": \"$data\"}";
+        }
     }
 }
 ?>
